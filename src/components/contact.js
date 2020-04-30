619a2e59b376a6e769200e4f0451a4ac5c18d1a6
unknown <amar.tauqeer@gmail.com>
2020-04-30 17:24:27 +0200

    Resume,about and home page finished

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,56 +1,47 @@
 import React, { Component } from 'react';
 import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
-
+import avtar from '../img/avatar.jpg';
 
 class Contact extends Component {
   render() {
-    return(
+    return (
       <div className="contact-body">
         <Grid className="contact-grid">
           <Cell col={6}>
-            <h2>Paul Hanna</h2>
+            <h2>Amar Tauqeer</h2>
             <img
-              src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+              src={avtar}
               alt="avatar"
-              style={{height: '250px'}}
-               />
-             <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+              style={{ height: '250px' }}
+            />
+            <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>To be able to work for an encouraging and stable company that will assist me in developing,improving, and obtaining the necessary skills in order to become the best engineer possible.</p>
 
           </Cell>
           <Cell col={6}>
             <h2>Contact Me</h2>
-            <hr/>
+            <hr />
 
             <div className="contact-list">
               <List>
                 <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
-                    <i className="fa fa-phone-square" aria-hidden="true"/>
-                    (123) 456-7890
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                    <i className="fa fa-phone-square" aria-hidden="true" />
+                    +43 688 64133065
                   </ListItemContent>
                 </ListItem>
 
                 <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
-                    <i className="fa fa-fax" aria-hidden="true"/>
-                    (123) 456-7890
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                    <i className="fa fa-envelope" aria-hidden="true" />
+                    amar.tauqeer@gmail.com
                   </ListItemContent>
                 </ListItem>
-
                 <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
-                    <i className="fa fa-envelope" aria-hidden="true"/>
-                    someone@example.com
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                    <i className="fa fa-github" aria-hidden="true" />
+                    https://github.com/AmarTauqeer
                   </ListItemContent>
                 </ListItem>
-
-                <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
-                    <i className="fa fa-skype" aria-hidden="true"/>
-                    MySkypeID
-                  </ListItemContent>
-                </ListItem>
-
 
               </List>
             </div>
--- src/components/skills.js
+++ src/components/skills.js
@@ -3,14 +3,13 @@ import { Grid, Cell, ProgressBar } from 'react-mdl';
 
 class Skills extends Component {
   render() {
-    return(
+    return (
       <Grid>
         <Cell col={12}>
-          <div style={{display: 'flex'}}>{this.props.skill} <ProgressBar style={{margin: 'auto', width: '75%'}} progress={this.props.progress} /> </div>
+          <div style={{ display: 'flex' }}>{this.props.skill} <ProgressBar style={{ margin: 'auto', width: '75%' }} progress={this.props.progress} /> </div>
         </Cell>
       </Grid>
     )
   }
 }
-
 export default Skills;
